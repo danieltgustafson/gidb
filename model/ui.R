d67b5984dfe14d43190f5294e140d6ba4e234bc8
Dan Gustafson <daniel.gustafson@lookout.com>
2014-11-24 09:51:02 -0800

    modify ticks

--- model/ui.R
+++ model/ui.R
@@ -11,12 +11,12 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
 
-      sliderInput('spend_fb',"Enter Facebook Spend",min=0,max=100000,value=1000,ticks=TRUE,step=5000),
-      sliderInput('spend_gg',"Enter Google/YMSN Spend",min=0,max=100000,value=1000,ticks=TRUE,step=5000),
-      sliderInput('spend_tr',"Enter Transit Spend",min=0,max=100000,value=1000,ticks=TRUE,step=5000),
-      sliderInput('spend_tv',"Enter TV Spend",min=0,max=100000,value=1000,ticks=TRUE,step=5000),
-      sliderInput('spend_ra',"Enter Radio Spend",min=0,max=100000,value=1000,ticks=TRUE,step=5000),
-      sliderInput('spend_pr',"Enter Print Spend",min=0,max=100000,value=1000,ticks=TRUE,step=5000),
+      sliderInput('spend_fb',"Enter Facebook Spend",min=0,max=75000,value=1000,ticks=TRUE,step=2500),
+      sliderInput('spend_gg',"Enter Google/YMSN Spend",min=0,max=75000,value=1000,ticks=TRUE,step=2500),
+      sliderInput('spend_tr',"Enter Transit Spend",min=0,max=75000,value=1000,ticks=TRUE,step=2500),
+      sliderInput('spend_tv',"Enter TV Spend",min=0,max=75000,value=1000,ticks=TRUE,step=2500),
+      sliderInput('spend_ra',"Enter Radio Spend",min=0,max=75000,value=1000,ticks=TRUE,step=2500),
+      sliderInput('spend_pr',"Enter Print Spend",min=0,max=75000,value=1000,ticks=TRUE,step=2500),
       dateInput('max_date',"Enter max model date"),
 
       #checkboxInput('site_check',"Check for statistically significant site Differences"),
