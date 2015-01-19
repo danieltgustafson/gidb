a7b465d2bf7b36e211077b6ba02f0e0a03c1ccbf
Dan Gustafson <daniel.gustafson@lookout.com>
2015-01-19 14:31:22 -0800

    updates to graphics

--- review/server.R
+++ review/server.R
@@ -15,14 +15,20 @@ shinyServer(function(input, output) {
   
 
 
-con=dbConnect(MySQL(),username='dgustafson',password='c3808v4m',host='54.69.26.113', port=3306)
+con=dbConnect(MySQL(),username='dgustafson',password='c3808v4m',host='localhost', port=3306)
   
   
  
 data<-reactive({
   dbGetQuery(con,"
-select site_name,count(distinct patient_id) inquiries,if(status<>'Randomized',year(`Pre-Screen Date`)*100+month(`Pre-Screen Date`),year(Randomization)*100+month(randomization)) rand_date,
-count(if(status<>'Failed Pre-Screen',patient_id,null)) as referrals, count(if(status='Randomized',patient_id,null)) as rands from gidb.endo1
+select a.site_name,rand_date,
+count(distinct if(100*year(`Pre-Screen Date`)+month(`Pre-Screen Date`)=rand_date,patient_id,null)) inquiries,
+count(distinct if(100*year(`Randomization`)+month(`Randomization`)=rand_Date,patient_id,null)) rands,
+count(distinct if(status<>'Failed Pre-Screen'and 100*year(`Pre-Screen Date`)+month(`Pre-Screen Date`)=rand_date,patient_id,null))as referrals, 
+count(if(`Screening Visit` >='2001-01-01'and 100*year(`Screening Visit`)+month(`Screening Visit`)=rand_date,patient_id,null)) screens, max(cost) as cost from gidb.endo1 a
+join (
+select year(start_date)* 100 + month(start_date) rand_date,site_id,sum(cost) cost from gidb.media 
+group by rand_date,site_id) b on a.site_id = b.site_id 
 group by rand_date,site_name")
 
 })
@@ -38,10 +44,14 @@ output$sites<-renderUI({
 data_lim<-reactive({
 	
 	a<-subset(data(),data()$rand_date>0&data()$site_name %in% input$sites)
-	a<-data.table(ddply(a,.(rand_date),summarize,rands=sum(rands),referrals=sum(referrals),inquiries=sum(inquiries)))
+	a<-data.table(ddply(a,.(rand_date),summarize,rands=sum(rands),referrals=sum(referrals),inquiries=sum(inquiries),
+		cost=sum(cost),screens=sum(screens)))
 	a[order(rand_date),cref:=cumsum(referrals)]
 	a[order(rand_date),cinq:=cumsum(inquiries)]
 	a[order(rand_date),crand:=cumsum(rands)]
+	a[order(rand_date),ccost:=cumsum(cost)]
+	a[order(rand_date),cscreen:=cumsum(screens)]
+
 })
 
 
@@ -55,7 +65,7 @@ data_lim<-reactive({
 			            title = list(text = 'Randomized')
 			        ),
 			        list(
-			            title = list(text = 'Referarals'), 
+			            title = list(text = input$measure), 
 			            opposite =TRUE
 			        )
 			    )
@@ -66,18 +76,95 @@ data_lim<-reactive({
 		    type = "column"
 		)
         theGraph$series(
-		    data = if(input$cumsum) {data_lim()$cref} else{data_lim()$referrals},
-		    name = "Referarals",
+		    data = 
+		    	if(input$measure=='Referrals')
+		    	{
+		    		if(input$cumsum)
+				     	{data_lim()$cref}
+				     else
+				     	{data_lim()$referrals}
+				}
+				else if(input$measure=='Inquiries')
+				{
+				     if(input$cumsum)
+				     	{data_lim()$cinq}
+				     else
+				     	{data_lim()$inquiries}
+				}
+				else
+				{
+					if(input$cumsum)
+				     	{data_lim()$cscreen}
+				     else
+				     	{data_lim()$screens}
+				},
+		    name = input$measure,
 		    type = "line",
 		    yAxis=1
 		)
-		theGraph$xAxis(categories=data_lim()$rand_date,labels=list(rotation=-45))
+		theGraph$xAxis(categories=data_lim()$rand_date,labels=list(rotation=-45,y=5))
         #theGraph$chart(zoomType = "xy")
         theGraph$addParams(dom='day2')
         
         return(theGraph)
       
   })
+ output$day3<-renderChart({
+      
+        theGraph <- Highcharts$new()
+		theGraph$yAxis(
+			    list(
+			        list(
+			            title = list(text = input$measure)
+			        ),
+			        list(
+			            title = list(text = 'Spend'), 
+			            opposite =TRUE
+			        )
+			    )
+			)
+        theGraph$series(
+		    data = if(input$measure=='Referrals')
+		    	{
+		    		if(input$cumsum)
+				     	{data_lim()$cref}
+				     else
+				     	{data_lim()$referrals}
+				}
+				else if(input$measure=='Inquiries')
+				{
+				     if(input$cumsum)
+				     	{data_lim()$cinq}
+				     else
+				     	{data_lim()$inquiries}
+				}
+				else
+				{
+					if(input$cumsum)
+				     	{data_lim()$cscreen}
+				     else
+				     	{data_lim()$screens}
+				},
+		    name = input$measure,
+		    type = "column"
+		)
+        theGraph$series(
+		    data = if(input$cumsum) {data_lim()$ccost} else{data_lim()$cost},
+		    name = "Spend",
+		    type = "area",
+		    fillOpacity = 0.2,
+		    yAxis=1
+		)
+		theGraph$xAxis(categories=data_lim()$rand_date,labels=list(rotation=-45,y=5))
+        #theGraph$chart(zoomType = "xy")
+        theGraph$addParams(dom='day3')
+        
+        return(theGraph)
+      
+  })
+contents<-reactive({
+	write(input$file1,'~/endo_data.xlsx')
+})
 
 output$table<-renderDataTable({
   data_lim()
--- review/ui.R
+++ review/ui.R
@@ -9,29 +9,41 @@ library(shiny)
 library(rCharts)
 
 shinyUI(fluidPage(
-
+  
   # Application title
   titlePanel("Overview"),
 
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
+      conditionalPanel(condition="input.tabs=='Main'",
       
-      uiOutput('sites'),
-      checkboxInput('selected','Select All',value=TRUE) 
+        uiOutput('sites'),
+        checkboxInput('selected','Select All',value=TRUE) 
       
 
     ),
+      conditionalPanel(condition="input.tabs=='Upload'",
+        fileInput('file1', 'Choose file to upload')
+    )),
 
 
     # Show a plot of the generated distribution
     mainPanel(
-      checkboxInput('cumsum','Check to cumulative sums'),
-      radioButtons('table','Show Table or Graphic?',list("Table"='table',"Graphic"='graphic')),
-      conditionalPanel(condition="input.table=='graphic'",
-        showOutput("day2",'highcharts')),
-      conditionalPanel(condition="input.table=='table'",
-        dataTableOutput("table"))
-    )
+      tabsetPanel(id="tabs",
+        tabPanel("Main",
+          checkboxInput('cumsum','Check for cumulative sums'),
+          radioButtons('table','Show Table or Graphic?',list("Table"='table',"Graphic"='graphic')),
+          conditionalPanel(condition="input.table=='graphic'",
+            selectInput('measure','Select Measurement',list('Inquiries'='Inquiries','Referrals'='Referrals','Screens'='Screens')),
+            showOutput("day2",'highcharts'),
+            showOutput("day3",'highcharts')),
+          conditionalPanel(condition="input.table=='table'",
+            dataTableOutput("table"))
+    ),
+        tabPanel("Upload",
+          dataTableOutput("preview"))
   )
+)
+)
 ))
