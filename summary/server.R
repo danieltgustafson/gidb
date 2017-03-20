d0bdddf3dbdee284714ca62ed7062e6af48054ec
danieltgustafson <danieltgustafson@gmail.com>
2017-03-20 09:05:39 -0700

    Update server.R

--- summary/server.R
+++ summary/server.R
@@ -13,13 +13,11 @@ library(plyr)
 getConnection <- function(group) {
 
   if (!exists('.connection', where=.GlobalEnv)) {
-    .connection <<- dbConnect(MySQL(),username='dgustafson',password='c3808v4m',
-    # host='54.69.26.113', port=3306)
+    .connection <<- dbConnect(MySQL(),username='',password='',
     host='localhost', port=3306)
   } else if (class(try(dbGetQuery(.connection, "SELECT 1"))) == "try-error") {
     dbDisconnect(.connection)
-    .connection <<- dbConnect(MySQL(),username='dgustafson',password='c3808v4m',
-    #  host='54.69.26.113', port=3306)
+    .connection <<- dbConnect(MySQL(),username='n,password='',
     host='localhost', port=3306)
   }
 
